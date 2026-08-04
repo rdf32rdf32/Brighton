af2672b793ca73646a5c80f9951241be2927cbf8
rdf32rdf32 <rdf32rdf32@gmail.com>
2026-08-04 11:09:27 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,55 +1,10 @@
-const CACHE = "albion-fan-hub-r62";
-const CORE = [
-  "./", "./index.html", "./site-r62.css", "./polish-r62.css",
-  "./albion-data-r62.js", "./app-r62.js", "./shootout-r62.js", "./r62-enhancements.js",
-  "./favicon.svg", "./albion-safe-graphic.svg", "./offline.html"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)));
-});
-
+// r63 retirement worker: remove Brighton/Albion offline behaviour, then unregister itself.
+const IS_BRIGHTON_CACHE = key => /brighton|albion|fan-hub/i.test(key);
+self.addEventListener("install", event => event.waitUntil(self.skipWaiting()));
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-  const url = new URL(event.request.url);
-  const sameOrigin = url.origin === self.location.origin;
-  const versionedAsset = sameOrigin && /(?:r62|favicon|albion-safe-graphic)/.test(url.pathname + url.search);
-
-  if (event.request.mode === "navigate") {
-    event.respondWith(
-      fetch(event.request)
-        .then(response => {
-          const copy = response.clone();
-          caches.open(CACHE).then(cache => cache.put(event.request, copy));
-          return response;
-        })
-        .catch(() => caches.match(event.request).then(hit => hit || caches.match("./offline.html")))
-    );
-    return;
-  }
-
-  if (versionedAsset) {
-    event.respondWith(
-      caches.match(event.request).then(hit => hit || fetch(event.request).then(response => {
-        const copy = response.clone();
-        caches.open(CACHE).then(cache => cache.put(event.request, copy));
-        return response;
-      }))
-    );
-    return;
-  }
-
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
-});
-
-self.addEventListener("message", event => {
-  if (event.data?.type === "SKIP_WAITING") self.skipWaiting();
+  event.waitUntil(Promise.all([
+    caches.keys().then(keys => Promise.all(keys.filter(IS_BRIGHTON_CACHE).map(key => caches.delete(key)))),
+    self.registration.unregister(),
+    self.clients.claim()
+  ]));
 });
